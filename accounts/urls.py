946ca3232dc18ccbbecdbded700a8e92742c2a8b
code-sum <yisugyeong@gmail.com>
2022-10-21 10:48:20 +0900

    User : login

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('signup/', views.signup, name='signup'),
     path('<int:pk>/', views.detail, name='detail'),
+    path('login/', views.login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 
 def index(request):
     users = get_user_model().objects.all()
@@ -28,3 +30,19 @@ def detail(request, pk):
         'user': user
     }
     return render(request, 'accounts/detail.html', context)
+
+def login(request):
+    if request.user.is_anonymous:
+        if request.method == 'POST':
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                auth_login(request, form.get_user()) 
+                return redirect('accounts:index')
+        else:
+            form = AuthenticationForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'accounts/login.html', context)
+    else:
+        return redirect('accounts:index')
